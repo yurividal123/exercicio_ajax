7e18ae8a138b830ecf4e7c16f21f17f21bb3d642
Yuri Vidal <yurividal123@hotmail.com>
2023-10-05 19:16:46 -0300

    fazendo o ajax e jquery do projeto

--- ajax.js
+++ ajax.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-    $.ajax(`https://api.github.com/users/LeandroSannt`).done(function(resposta) {
-        console.log(resposta);
+    $('#btn-buscar').click(function() {
+        const buscar = $('#buscar').val();
+        const users = `https://api.github.com/users/${buscar}`;
+
+        $.ajax(users).done(function(resposta){
+            const avatar = resposta.avatar_url;
+            const name = resposta.name;
+            const followers = resposta.followers;
+            const following = resposta.following;
+
+            const avatar1 = `${avatar}`;
+            $('#avatar_url').val(avatar1)
+        })
     })
 })
