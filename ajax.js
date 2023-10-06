bc9669515b7e1423b799ef66aa33008156fc53b4
Yuri Vidal <yurividal123@hotmail.com>
2023-10-06 16:16:19 -0300

    finalizado o exercicio

--- ajax.js
+++ ajax.js
@@ -1,16 +1,35 @@
 $(document).ready(function() {
     $('#btn-buscar').click(function() {
         const buscar = $('#buscar').val();
-        const users = `https://api.github.com/users/${buscar}`;
+        const endpoint = `https://api.github.com/users/${buscar}`;
 
-        $.ajax(users).done(function(resposta){
+        $.ajax(endpoint).done(function(resposta) {
             const avatar = resposta.avatar_url;
             const name = resposta.name;
             const followers = resposta.followers;
             const following = resposta.following;
+            const repos = resposta.public_repos;
+            const url = resposta.html_url;
 
-            const avatar1 = `${avatar}`;
-            $('#avatar_url').val(avatar1)
+
+
+            const avatarItem = `${avatar}`;
+            const nameItem = `${name}`;
+            const followersItem = `${followers}`;
+            const followingItem = `${following}`;
+            const reposItem = `${repos}`
+            const urlItem = `${url}`;
+
+
+            document.querySelector('#name').innerHTML = nameItem;
+            document.querySelector('#followers').innerHTML = followersItem;
+            document.querySelector('#following').innerHTML = followingItem;
+            document.querySelector('#repos').innerHTML = reposItem;
+            document.querySelector('#avatar').src = avatarItem;
+            document.querySelector('#url').src = urlItem;
+            
+
         })
     })
 })
+
